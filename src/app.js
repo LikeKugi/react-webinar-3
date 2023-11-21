21894a8484288118a31e4994f84ddfcc1ac30127
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-21 15:51:57 +0300

    feat: add targeting counter

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {createElement} from './utils.js';
 import './styles.css';
 
 /**
@@ -27,6 +26,7 @@ function App({store}) {
                    onClick={() => store.selectItem(item.code)}>
                 <div className='Item-code'>{item.code}</div>
                 <div className='Item-title'>{item.title}</div>
+                {item.targeted && <div className="Item-targeted">Выделяли {item.targeted} раз</div>}
                 <div className='Item-actions'>
                   <button onClick={() => store.deleteItem(item.code)}>
                     Удалить
