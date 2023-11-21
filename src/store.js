82ca6cd2e89a5518d2e51a1f9a3b4c0c770babea
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-21 16:12:20 +0300

    feat: add unique index for elements of state

--- src/store.js
+++ src/store.js
@@ -5,6 +5,10 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+    this.setState({
+      ...this.state,
+      unique_idx: this.makeUniqueIndex()
+    });
   }
 
   /**
@@ -17,7 +21,7 @@ class Store {
     // Возвращается функция для удаления добавленного слушателя
     return () => {
       this.listeners = this.listeners.filter(item => item !== listener);
-    }
+    };
   }
 
   /**
@@ -42,10 +46,12 @@ class Store {
    * Добавление новой записи
    */
   addItem() {
+    const unique_idx = this.makeUniqueIndex(this.state.unique_idx);
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
+      unique_idx,
+      list: [...this.state.list, {code: unique_idx, title: "Новая запись"}]
+    });
   };
 
   /**
@@ -56,7 +62,7 @@ class Store {
     this.setState({
       ...this.state,
       list: this.state.list.filter(item => item.code !== code)
-    })
+    });
   };
 
   /**
@@ -70,14 +76,29 @@ class Store {
         if (item.code === code) {
           item.selected = !item.selected;
           if (item.selected) {
-            item.targeted = item.targeted ? item.targeted + 1 : 1
+            item.targeted = item.targeted ? item.targeted + 1 : 1;
           }
         } else {
           item.selected = false;
         }
         return item;
       })
-    })
+    });
+  }
+
+  /**
+   * Создание уникального индекса
+   * @param {number?} initial
+   * @returns {number}
+   */
+  makeUniqueIndex(initial) {
+    if (initial) {
+      return initial + 1;
+    }
+    if (this.state.list.length > 0 && this.state.list[0].code) {
+      return Math.max(...this.state.list.map(el => el.code)) + 1;
+    }
+    return 1;
   }
 }
 
