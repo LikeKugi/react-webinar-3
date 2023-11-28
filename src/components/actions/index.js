22d0aeea4561b91126aee11e14faf236a9c6b227
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-28 22:00:36 +0300

    feat: add actions container

---
+++ src/components/actions/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import './style.css'
+
+const Actions = ({children}) => {
+  return (
+    <div className="Actions">
+      {children}
+    </div>
+  );
+};
+
+Actions.propTypes = {
+  children: PropTypes.node,
+}
+
+
+export default Actions;
