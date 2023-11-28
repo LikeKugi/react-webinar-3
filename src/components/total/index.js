fbced3a15f1ee366ecc089cb031ddf1c547c0d8a
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-28 22:00:52 +0300

    feat: add total price container

---
+++ src/components/total/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css'
+import PropTypes from "prop-types";
+
+const Total = ({children}) => {
+  return (
+    <div className="Total">
+      {children}
+    </div>
+  );
+}
+
+Total.propTypes = {
+  children: PropTypes.node,
+}
+
+export default Total;
