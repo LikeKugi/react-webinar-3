c50afb4488242a1ff1103817dc1199bb18586da4
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-12-14 22:11:25 +0300

    fix: fix behavior on token error

--- src/app/profile/index.js
+++ src/app/profile/index.js
@@ -6,10 +6,13 @@ import Navigation from "../../containers/navigation";
 import Spinner from "../../components/spinner";
 import ProfileCard from "../../components/profile-card";
 import {Navigate} from "react-router-dom";
-import {getCookie} from "../../utils";
+import {deleteCookie, getCookie} from "../../utils";
+import useStore from "../../hooks/use-store";
+import {useEffect} from "react";
 
 function Profile() {
   const {t} = useTranslate();
+  const store = useStore();
   const token = getCookie('token');
 
   const select = useSelector(state => ({
@@ -18,9 +21,18 @@ function Profile() {
     name: state.user.user.profile.name,
     phone: state.user.user.profile.phone,
     email: state.user.user.email,
+    error: state.user.error,
   }));
 
-  if (!token) {
+  useEffect(() => {
+    const load = async () => {
+      await store.actions.user.selfUser(token)
+    }
+    load();
+  }, [])
+
+  if (!token || select.error) {
+    deleteCookie('token')
     return <Navigate to={"/login"}/>;
   }
 
