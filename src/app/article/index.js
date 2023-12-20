89ffabe5447949c9bbbaeae003689126be4c09de
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-12-20 12:36:12 +0300

    feat: add loading comments

--- src/app/article/index.js
+++ src/app/article/index.js
@@ -1,18 +1,20 @@
-import {memo, useCallback, useMemo} from 'react';
-import {useParams} from 'react-router-dom';
-import useStore from '../../hooks/use-store';
-import useTranslate from '../../hooks/use-translate';
-import useInit from '../../hooks/use-init';
-import PageLayout from '../../components/page-layout';
-import Head from '../../components/head';
-import Navigation from '../../containers/navigation';
-import Spinner from '../../components/spinner';
-import ArticleCard from '../../components/article-card';
-import LocaleSelect from '../../containers/locale-select';
-import TopHead from '../../containers/top-head';
-import {useDispatch, useSelector} from 'react-redux';
-import shallowequal from 'shallowequal';
-import articleActions from '../../store-redux/article/actions';
+import {memo, useCallback} from "react";
+import {useParams} from "react-router-dom";
+import useStore from "../../hooks/use-store";
+import useTranslate from "../../hooks/use-translate";
+import useInit from "../../hooks/use-init";
+import PageLayout from "../../components/page-layout";
+import Head from "../../components/head";
+import Navigation from "../../containers/navigation";
+import Spinner from "../../components/spinner";
+import ArticleCard from "../../components/article-card";
+import LocaleSelect from "../../containers/locale-select";
+import TopHead from "../../containers/top-head";
+import {useDispatch, useSelector} from "react-redux";
+import shallowequal from "shallowequal";
+import articleActions from "../../store-redux/article/actions";
+import commentsActions from "../../store-redux/comments/actions";
+import CommentsList from "../../containers/comments-list";
 
 function Article() {
   const store = useStore();
@@ -22,9 +24,10 @@ function Article() {
 
   const params = useParams();
 
-  useInit(() => {
+  useInit(async () => {
     //store.actions.article.load(params.id);
     dispatch(articleActions.load(params.id));
+    dispatch(commentsActions.load(params.id));
   }, [params.id]);
 
   const select = useSelector(state => ({
@@ -37,7 +40,7 @@ function Article() {
   const callbacks = {
     // Добавление в корзину
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
-  }
+  };
 
   return (
     <PageLayout>
@@ -47,8 +50,11 @@ function Article() {
       </Head>
       <Navigation/>
       <Spinner active={select.waiting}>
-        <ArticleCard article={select.article} onAdd={callbacks.addToBasket} t={t}/>
+        <ArticleCard article={select.article}
+                     onAdd={callbacks.addToBasket}
+                     t={t}/>
       </Spinner>
+      <CommentsList articleId={params.id}/>
     </PageLayout>
   );
 }
